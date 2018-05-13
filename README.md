771bb2bd17bd4f2d485191229d934f437b79794d
Dominik Schilling <dominikschilling+git@gmail.com>
2018-05-13 14:35:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Disable Privacy Tools
+Contributors:      ocean90  
+Tags:              privacy, gdpr, tools, core 
+Requires at least: 4.9  
+Tested up to:      5.0  
+Requires PHP:      5.2  
+Stable tag:        1.0  
+License:           GPLv2 or later  
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html  
+
+Removes the tools for creating a privacy policy and exporting/erasing personal data.
+
+## Description
+
+WordPress 4.9.6 added a few tools to help you creating and displaying a privacy policy or assist with requests for exporting/erasing personal data.  
+Depending on your location or type of install, e.g. a multisite, you may  have already have taken measures to comply with existing privacy laws or/and you do not need them at all.
+
+With this plugin, all the new admin screens will be disabled to prevent cluttering your admin with more unused items. It also prevents showing you the feature pointer if you install this plugin before the update.
---
+++ disable-privacy-tools.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Plugin Name: Disable Privacy Tools
+ * Version: 1.0
+ * Description: Disables core's privacy tools including tools for exporting/erasing personal data.
+ * Author: Dominik Schilling
+ * Author URI: https://dominikschilling.de/
+ * Plugin URI: https://github.com/ocean90/wp-disable-privacy-tools
+ * License: GPLv2 or later
+ *
+ *    Copyright (C) 2018 Dominik Schilling
+ *
+ *    This program is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public License
+ *    as published by the Free Software Foundation; either version 2
+ *    of the License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @package disable-privacy-tools
+ */
+
+/**
+ * Removes required user's capabilities for core privacy tools by adding the
+ * `do_not_allow` capability.
+ *
+ *  - Disables the feature pointer.
+ *  - Removes the Privacy and Export/Erase Personal Data admin menu items.
+ *  - Disables the privacy policy guide and update bubbles.
+ *
+ * @param string[] $caps    Array of the user's capabilities.
+ * @param string   $cap     Capability name.
+ * @return string[] Array of the user's capabilities.
+ */
+function ds_disable_core_privacy_tools( $caps, $cap ) {
+	switch ( $cap ) {
+		case 'export_others_personal_data':
+		case 'erase_others_personal_data':
+		case 'manage_privacy_options':
+			$caps[] = 'do_not_allow';
+			break;
+	}
+
+	return $caps;
+}
+add_filter( 'map_meta_cap', 'ds_disable_core_privacy_tools', 10, 2 );
+
+/**
+ * Short circuits the option for the privacy policy page to always return 0.
+ *
+ * The option is used by get_privacy_policy_url() among others.
+ */
+add_filter( 'pre_option_wp_page_for_privacy_policy', '__return_zero' );
